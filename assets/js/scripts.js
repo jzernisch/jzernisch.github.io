3a3a19abe11cf3f34969940832020140ebaf1d90
Jan Zernisch <jan.zernisch@invision.de>
2022-11-28 22:47:57 +0100

    Add license information

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,9 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+
 window.addEventListener('DOMContentLoaded', event => {
 
     const sideNav = document.body.querySelector('#sideNav');
